8853b870eecf81a9554df010ad75446c575c963d
sawon-saiful <sasawonsaiful@gmail.com>
2024-02-20 23:43:01 +0600

    some section update

---
+++ style/busticket.js
@@ -0,0 +1,21 @@
+// Get the div element by its id
+var cardDiv = document.getElementById('myCard');
+
+// Set width and height attributes using JavaScript
+cardDiv.style.width = '1170px';
+cardDiv.style.height = '400px';
+
+
+window.addEventListener('load', function () {
+    var section = document.getElementById('mySection');
+    section.style.width = '1170px';
+    section.style.height = '1056px';
+});
+
+
+// Get the div element
+var divElement = document.getElementById("myDiv");
+
+// Set the width and height
+divElement.style.width = "573px";
+divElement.style.height = "220px";
---
+++ style/my-card.js
@@ -0,0 +1,6 @@
+// Get the div element by its id
+var cardDiv = document.getElementById('my-card');
+
+// Add custom classes to adjust width and height
+cardDiv.classList.add('custom-width');
+cardDiv.classList.add('custom-height');
